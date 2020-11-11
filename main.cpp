9e9c83cc8ab4935a0378e6c77150f3ca9daed635
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-11-11 11:24:34 +0800

    main: Scheduler schedules job and can wait for all jobs to be finished

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <condition_variable>
 #include <exception>
 #include <iostream>
 #include <mutex>
@@ -13,23 +14,53 @@ class Scheduler
 public:
   Scheduler(size_t size, Error error);
   void schedule(const Job job, long time);
+  void wait();
 
 private:
   size_t mSize;
+  size_t mCount;
   Error mError;
-  std::mutex mMutx;
+  std::mutex mMutex;
+  std::condition_variable mCondition;
 };
 
 Scheduler::Scheduler(size_t size, Error error)
 : mSize(size)
 , mError(error)
+, mCount(0)
 {}
 
 void Scheduler::schedule(const Job job, long time)
 {
-  std::thread t(job);
+  std::unique_lock<std::mutex> lock(this->mMutex);
+  mCondition.wait(lock, [this] { return this->mCount < this->mSize; });
+  mCount++;
 
-  t.join();
+  auto j = std::make_shared<Job>(job);
+
+  std::thread t(
+    [time, j, this] {
+      std::this_thread::sleep_for(std::chrono::microseconds(time));
+      try {
+        (*j)();
+      } catch (const std::exception &e) {
+        this->mError(e);
+      } catch (...) {
+        this->mError(std::runtime_error("Unknown error"));
+      }
+
+      mCondition.notify_one();
+      mCount--;
+    }
+  );
+
+  t.detach();
+}
+
+void Scheduler::wait()
+{
+  std::unique_lock<std::mutex> lock(this->mMutex);
+  mCondition.wait(lock, [this] { return this->mCount == 0; });
 }
 
 int main(int argc, char **argv)
@@ -38,9 +69,15 @@ int main(int argc, char **argv)
     std::cout << "Error: " << e.what() << std::endl;
   });
 
-  scheduler.schedule([]() {
+  scheduler.schedule([] {
     std::cout << "Hello" << std::endl;
+  }, 5000000);
+
+  scheduler.schedule([] {
+    std::cout << "Yo!" << std::endl;
   }, 1000000);
+
+  scheduler.wait();
 
   return 0;
 }
